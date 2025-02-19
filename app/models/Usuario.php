ba2eaa5b8a07b53c4e285644c9e13f1884bd9c68
paul0queiroz <queirozp0807@gmail.com>
2025-02-18 22:57:08 -0300

    Add Usuario class with database connection and initial method for listing users

--- app/models/Usuario.php
+++ app/models/Usuario.php
@@ -1,3 +1,19 @@
 <?php
+require_once "config/database.php";
 
+class Usuario {
+    private $conn;
+    private $table_name = "usuarios";
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn =$database->getConnection();
+    }
+
+    //Metodo para listar todos os usuarios
+    public function listarUsuarios(){
+        $query = ""
+    }
+    }
+}
 ?>
